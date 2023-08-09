2665055fa57d2044f3f39a159d8559124ec40675
Ayaryga <alekseyyaryga73@gmail.com>
2023-08-09 23:32:46 +0300

    number 3

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 console.log('hello world');
+
+const textSeach = document.querySelector('#title');
+const styleChange = document.querySelector('#change');
+
+textSeach.addEventListener('click', function(){
+    styleChange.style.color = 'red'
+});
+
+styleChange.addEventListener('click', function(){
+    styleChange.style.color = 'green';
+    
+})
